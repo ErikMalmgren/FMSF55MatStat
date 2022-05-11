b47e213d4bea3d4eca9294ada21e3c91ec5ae53d
ErikM <er8540ma-s@student.lu.se>
2022-05-11 13:18:57 +0200

    Knegat lite till

--- projekt.m
+++ projekt.m
@@ -1,7 +1,13 @@
 %Detta blir pog
 load('huspriser55.mat')
 hist(pris)
+figure
 cdfplot(pris)
 billigast = prctile(pris, 90) % 10% dyraste husen
 dyrast = prctile(pris,20) %20% billigaste husen
 under100 = length(pris(pris<100000))/length(pris) %Hur stor andel av husen kostar mindre än 100 000 i Saratoga County
+pd = fitdist(pris, 'Normal')
+xNorm = [0:.1:370000];
+yNorm = normpdf(xNorm, 164583, 68267.9);
+figure
+plot(xNorm, yNorm)
